bb8d55d6cf4f5ab9f298c337428d2aa4884f03d2
magma69 <magmadev8@gmail.com>
2023-10-12 12:54:11 -0400

    more importand

--- scripts/index.js
+++ scripts/index.js
@@ -132,11 +132,3 @@ const webSocket = new WebSocket('ws://localhost:8080');
 
 
 
-function hoverEffect(id) {
-    let element = document.getElementById(id)
-    if (id == "setting") {
-        element.innerHTML = `<i class='bx bx-cog bx-tada' style='color:#ffffff;width: 24px; height: 24px;' ></i>`
-    } else if (id == "focus") {
-        element.innerHTML = `<i class='bx bxs-shield-minus bx-tada' style='color:#fffffF; width: 24px; height: 24px;'></i>`
-    }
-}
